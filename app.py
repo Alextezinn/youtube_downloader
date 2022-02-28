5f3a98c0b7f395748057bfea9ea722d717967aff
Alextezinn <65595850+Alextezinn@users.noreply.github.com>
2022-02-28 17:38:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from youtube_downloader.youtube import YouTubeVideoDownloaderHighestResolution
 
 
 if __name__ == "__main__":
-    youtube = YouTubeVideoDownloaderHighestResolution("https://www.youtube.com/watch?v=8Fl6d_fSRNs")
+    youtube = YouTubeVideoDownloaderHighestResolution("youtube url video")
     youtube.download_video()
