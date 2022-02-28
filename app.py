c12054ac685141b7915910d7b82294ea0120661b
Alextezinn <alexandertezin1999@yandex.ru>
2022-02-28 20:34:51 +0000

    bug fixes

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from youtube_downloader.youtube import YouTubeVideoDownloaderHighestResolution
 
 
 if __name__ == "__main__":
-    youtube = YouTubeVideoDownloaderHighestResolution("url на видео с ютуб")
+    youtube = YouTubeVideoDownloaderHighestResolution("https://www.youtube.com/watch?v=8Fl6d_fSRNs")
     youtube.download_video()
